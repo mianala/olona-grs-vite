4d395d5f409f2e7c6f9efffe24539960375c7fbc
nyvoahary <nekenanyvoahary@gmail.com>
2022-06-30 14:27:43 +0300

    setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from "react"
-import ReactDOM from "react-dom/client"
-import App from "./App"
-import "./index.sass"
+import { StrictMode } from "react";
+
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
+import App from "./App";
+import "./index.sass";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <StrictMode>
+  <Router>
     <App />
-  </StrictMode>
-)
+  </Router>
+);
