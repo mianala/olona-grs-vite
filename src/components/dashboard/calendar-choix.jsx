4ca4ee373683eaea8fc3c347e0a5b6371e3ada17
Manasina <tianiazymarindrano@gmail.com>
2022-07-05 17:51:56 +0700

    autocomplete chossisez compte

--- src/components/dashboard/calendar-choix.jsx
+++ src/components/dashboard/calendar-choix.jsx
@@ -5,40 +5,53 @@ const CalendarChoix = () => {
   const [users, setUsers] = useState([])
   const [search, setSearch] = useState("")
   const [matched, setMatched] = useState([])
-
   const searchRef = useRef()
   useEffect(() => {
     const fetchUsers = async () => {
       const res = await fetch("https://randomuser.me/api/?results=10")
       const data = await res.json()
-      const result = data.results
+      const result = data.results.map((data, index) => {
+        return { ...data, img: personImg[index].urls.small }
+      })
       setUsers(result)
+      setMatched(result)
     }
     fetchUsers()
   }, [])
+  useEffect(() => {
+    const matched = users.filter((user) => {
+      return `${user.name.first} ${user.name.last}`
+        .toLowerCase()
+        .includes(search.toLowerCase())
+    })
+    setMatched(matched)
+  }, [search])
   return (
     <>
-      <input
-        type="text"
-        placeholder="Type here"
-        className="input input-bordered input-info w-full max-w-xs"
-        ref={searchRef}
-        value={search}
-        onChange={(event) => setSearch(event.currentTarget.value)}
-      />
-      <div>
-        {users?.map(({ login: { uuid }, name: { first, last } }) => {
+      <div class="form-control w-full max-w-xs">
+        <label class="label">
+          <span class="label-text">Choisissez une compte</span>
+          <span class="label-text-alt"></span>
+        </label>
+        <input
+          type="text"
+          placeholder="Type here"
+          className="input input-bordered input-info w-full max-w-xs outline-none"
+          ref={searchRef}
+          value={search}
+          onChange={(event) => setSearch(event.currentTarget.value)}
+        />
+      </div>
+      <div className="my-5">
+        {matched?.map(({ login: { uuid }, name: { first, last }, img }) => {
           return (
-            <div className="flex items-center space-x-3" key={uuid}>
+            <div
+              className="flex items-center space-x-3 cursor-pointer hover:bg-slate-200 p-2 rounded-lg"
+              key={uuid}
+            >
               <div className="avatar">
                 <div className="mask mask-squircle w-12 h-12">
-                  <img
-                    src={
-                      personImg[Math.floor(Math.random() * personImg.length)]
-                        .urls.small
-                    }
-                    alt="Avatar Account"
-                  />
+                  <img src={img} alt="Avatar Account" />
                 </div>
               </div>
               <div>
