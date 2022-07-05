690acd70d00eea4faa29a4e83fa9153a6c31d324
mianala <mianalaloharano@gmail.com>
2022-07-05 17:44:54 +0300

    add: tasksLayout

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,30 @@
-import "./App.sass"
-import { Routes, Route } from "react-router-dom"
-import Layout from "./components/layout/layout"
-import SignIn from "./pages/auth/signin"
-import SignUp from "./pages/auth/signup"
-import AddInfo from "./pages/insription/add-info"
-import Hero from "./pages/hero/hero"
-import { useContext, useEffect } from "react"
-import ThemeContext from "./context/theme-context"
-import AuthContext from "./context/auth-context"
-import NotificationContext from "./context/notification-context"
-import { toast, ToastContainer } from "react-toastify"
-import { useLocation } from "react-router-dom"
-import { AnimatePresence } from "framer-motion"
-import Newfeed from "./components/new-feed/new-feed"
-import DashBoard from "./components/dashboard/dashboard"
+import "./App.sass";
+import { Routes, Route } from "react-router-dom";
+import Layout from "./components/layout/layout";
+import SignIn from "./pages/auth/signin";
+import SignUp from "./pages/auth/signup";
+import AddInfo from "./pages/insription/add-info";
+import Hero from "./pages/hero/hero";
+import { useContext, useEffect } from "react";
+import ThemeContext from "./context/theme-context";
+import AuthContext from "./context/auth-context";
+import NotificationContext from "./context/notification-context";
+import { toast, ToastContainer } from "react-toastify";
+import { useLocation } from "react-router-dom";
+import { AnimatePresence } from "framer-motion";
+import Newfeed from "./components/new-feed/new-feed";
+import DashBoard from "./components/dashboard/dashboard";
+import Tasks from "./components/task/tasks";
 
 function App() {
-  const { theme, nightMode } = useContext(ThemeContext)
-  const { isLoggedIn } = useContext(AuthContext)
-  const { notification } = useContext(NotificationContext)
-  const location = useLocation()
+  const { theme, nightMode } = useContext(ThemeContext);
+  const { isLoggedIn } = useContext(AuthContext);
+  const { notification } = useContext(NotificationContext);
+  const location = useLocation();
   useEffect(() => {
-    if (!notification.type) return
-    toast[notification.type](notification.message)
-  }, [notification])
+    if (!notification.type) return;
+    toast[notification.type](notification.message);
+  }, [notification]);
   return (
     <div data-theme={nightMode ? "dark" : "light"}>
       <AnimatePresence>
@@ -32,6 +33,7 @@ function App() {
           <Route path="" element={<Layout />}>
             <Route path="" element={<DashBoard />} />
             <Route path="feed" element={<Newfeed />} />
+            <Route path="tasks" element={<Tasks />} />
           </Route>
           <Route path="/ajout-info" element={<AddInfo />} />
           <Route path="auth">
@@ -42,10 +44,10 @@ function App() {
       </AnimatePresence>
       <ToastContainer autoClose={5000} limit={4} draggable pauseOnHover />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
 
 {
   /* <Route path="/" element={<Navigate replace to="/auth/connexion" />} /> */
---
+++ src/components/task/tasks.jsx
@@ -0,0 +1,115 @@
+import React from "react";
+import { useEffect, useState } from "react";
+import classes from "./tasklayout.module.sass";
+
+export default function Tasks() {
+  const [users, setUser] = useState(null);
+  useEffect(() => {
+    const getUser = async () => {
+      const usersReq = await fetch(
+        "https://jsonplaceholder.typicode.com/users"
+      );
+      const response = await usersReq.json();
+      setUser(response);
+    };
+    getUser();
+  }, []);
+  return (
+    <div className="flex flex-col gap-5">
+      <div className="flex gap-2">
+        <h1 className="font-extrabold text-xl">TACHES A FAIRE</h1>
+        <span className={`${classes.spacer}`}></span>
+        <input
+          type="date"
+          placeholder="Type here"
+          class="input input-bordered w-85 max-w-xs"
+        />
+        <input
+          type="date"
+          placeholder="Type here"
+          class="input input-bordered w-85 max-w-xs"
+        />
+      </div>
+      <hr />
+      <div class="overflow-x-auto">
+        <table class="table w-full">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Firstname</th>
+              <th>Name</th>
+              <th>Job</th>
+              <th>Favorite Color</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users?.map(({ id, name, username, email }) =>
+              id % 2 === 0 ? (
+                <tr className="active">
+                  <td>{id}</td>
+                  <td>{name}</td>
+                  <td>{username}</td>
+                  <td>{email}</td>
+                </tr>
+              ) : (
+                <tr key={id}>
+                  <td>{id}</td>
+                  <td>{name}</td>
+                  <td>{username}</td>
+                  <td>{email}</td>
+                </tr>
+              )
+            )}
+          </tbody>
+        </table>
+      </div>
+      <div className="flex gap-2">
+        <h1 className="font-extrabold text-xl">TACHES A FAIRE</h1>
+        <span className={`${classes.spacer}`}></span>
+        <input
+          type="date"
+          placeholder="Type here"
+          class="input input-bordered w-85 max-w-xs"
+        />
+        <input
+          type="date"
+          placeholder="Type here"
+          class="input input-bordered w-85 max-w-xs"
+        />
+      </div>
+      <hr />
+      <div class="overflow-x-auto">
+        <table class="table w-full">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Firstname</th>
+              <th>Name</th>
+              <th>Job</th>
+              <th>Favorite Color</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users?.map(({ id, name, username, email }) =>
+              id % 2 === 0 ? (
+                <tr className="active">
+                  <td>{id}</td>
+                  <td>{name}</td>
+                  <td>{username}</td>
+                  <td>{email}</td>
+                </tr>
+              ) : (
+                <tr key={id}>
+                  <td>{id}</td>
+                  <td>{name}</td>
+                  <td>{username}</td>
+                  <td>{email}</td>
+                </tr>
+              )
+            )}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+}
