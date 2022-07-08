cb2df9416a2f071a3c356b734227470729042857
mianala <mianalaloharano@gmail.com>
2022-07-08 17:40:46 +0300

    fix: new-feed card

--- src/pages/new-feed/new-feed.jsx
+++ src/pages/new-feed/new-feed.jsx
@@ -24,17 +24,29 @@ export default function Newfeed() {
               ease: [0.61, 1, 0.88, 1],
             },
           }}
+          whileHover={{ scale: 1.02 }}
           exit={{ opacity: 0 }}
           className="card bg-base-100 shadow-xl mt-2 pt-2"
         >
           <figure>
-            <img src={image} alt="Shoes" className="w-20 h-20" />
+            <img src={image} alt="Shoes" className="w-full" />
           </figure>
           <div className="card-body">
-            <h2 className="card-title">{title.substring(0, 10)}</h2>
-            <p>{description.substring(0, 30)}</p>
-            <div className="card-actions justify-start">
-              <button className="btn btn-primary">Buy Now</button>
+            <h2 className="card-title">{title}</h2>
+
+            <p>{description}</p>
+
+            <p className="text-gray-400">- 25 Juillet 2021 -</p>
+          </div>
+          <div className="action bg-gray-100 p-3 flex items-center gap-2">
+            <div className="avatar">
+              <div className="w-14 rounded-full">
+                <img src="https://source.unsplash.com/random/?portrait,avatar" />
+              </div>
+            </div>
+            <div className="flex flex-col gap-1">
+              <p className="font-bold">John Doe</p>
+              <p className="text-gray-400">Titre</p>
             </div>
           </div>
         </motion.div>
