84221a1aac02dbfdb11ea9d7b51f3751f85c3148
mianala <mianalaloharano@gmail.com>
2022-07-14 11:25:39 +0300

    fix: notification layout

--- src/pages/notification/notification-list.jsx
+++ src/pages/notification/notification-list.jsx
@@ -1,5 +1,62 @@
 import React from "react"
+import { FiMoreVertical } from "react-icons/fi"
+import notifications from "../../assets/fakedata/notifications.json"
 
 export default function NotificationList() {
-  return <div>notification-list</div>
+  const parseDate = (input) => {
+    return new Date(input).toISOString().split("T")[0]
+  }
+  return (
+    <div className="mx-40 flex-col gap-5 flex">
+      <div className="flex justify-between">
+        <span>Liste des notifications</span>
+        <FiMoreVertical />
+      </div>
+      <hr />
+      <div className="flex gap-3">
+        <div className="badge badge-primary">Tous</div>
+        <div className="badge badge-ghost">non lus</div>
+      </div>
+      <div className="flex flex-col gap-3 px-5">
+        {notifications.map((notification, index) => (
+          <div key={index} className="flex items-center gap-2">
+            <div className="w-12 h-12 relative">
+              <img
+                className="w-12 h-12 rounded-full mx-auto"
+                src={notification.avatar}
+                alt="user-avatar"
+              />
+              <span
+                className={`absolute w-4 h-4 ${
+                  notification.unread ? "bg-green-400" : "bg-gray-400"
+                }   rounded-full right-0 bottom-0 border-2 border-white`}
+              ></span>
+            </div>
+            <div className="flex flex-col">
+              <div>
+                <span className="font-bold">{notification.full_name}</span>{" "}
+                {(notification.user_action == "Liked") |
+                (notification.user_action == "Following") ? (
+                  <>
+                    <span>{notification.user_action}</span>
+                    {" your"}{" "}
+                  </>
+                ) : (
+                  <>
+                    <span>{notification.user_action}</span>
+                    {" to your"}{" "}
+                  </>
+                )}
+                <span className="font-semibold italic">
+                  {notification.cible}
+                </span>
+              </div>
+              <div>"{notification.text}"</div>
+              <div>{parseDate(notification.duration)}</div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
 }
