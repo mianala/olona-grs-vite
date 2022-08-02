d2df6fd89235eb650e58168851703895bf6a5bbc
nyvoahary <nekenanyvoahary@gmail.com>
2022-08-02 16:39:05 +0300

    fix: social mediaList

--- src/components/layout/social-list.jsx
+++ src/components/layout/social-list.jsx
@@ -1,38 +1,59 @@
 import { socialIcon } from "../neccessities/icon"
+import { BsFilterSquare } from "react-icons/bs"
 
-const SocialMedia = [{
+const SocialMedias = [
+  {
     id: 1,
-    social_media: "facebook",
+    social_media: "Facebook",
     icon: socialIcon.facebookIcon,
-},
-{
+  },
+  {
     id: 2,
-    social_media: "twitter",
+    social_media: "Twitter",
     icon: socialIcon.twitterIcon,
-},{
+  },
+  {
     id: 3,
     social_media: "Youtube",
     icon: socialIcon.youtubeIcon,
-},
-{
+  },
+  {
     id: 5,
     social_media: "Linkedin",
     icon: socialIcon.linkedinIcon,
-},
-{
+  },
+  {
     id: 6,
-    social_media: "Upwork",
-    icon: socialIcon.upworkIcon,
-}
+    social_media: "Pinterest",
+    icon: socialIcon.pinterestIcon,
+  },
 ]
 
-export default  function SocialList() {
-return (<div className="flex flex-wrap gap-5 items-center my-2">
-    {SocialMedia.map(social => {
-        return <div key={social.id} className="flex gap-3 items-center btn border-none btn-outline">
-            <img src={social.icon} alt={social.social_media} className="h-8 w-8" />
+export default function SocialList() {
+  return (
+    <div className="flex flex-wrap gap-1 items-center my-2 mb-5">
+      <div
+        key={0}
+        className="flex gap-1 items-center btn border-none btn-outline"
+      >
+        <BsFilterSquare className="h-6 w-6" />
+        <span className="text-md">Tous</span>
+      </div>
+      {SocialMedias.map((social) => {
+        return (
+          <div
+            key={social.id}
+            className="flex gap-1 items-center btn border-none btn-outline"
+          >
+            <img
+              src={social.icon}
+              alt={social.social_media}
+              className="h-6 w-6"
+            />
             <span className="text-md">{social.social_media}</span>
-        </div>
-    })}
-</div>)
+          </div>
+        )
+      })}
+    </div>
+  )
 }
--- src/components/neccessities/icon.jsx
+++ src/components/neccessities/icon.jsx
@@ -1,19 +1,15 @@
 import facebookIcon from "../../assets/social/facebook.svg"
-import tiktokIcon from "../../assets/social/tiktok.svg"
-import instagramIcon from "../../assets/social/instagram.svg"
-import twitterIcon from "../../assets/social/twitter.svg"
-import youtubeIcon from "../../assets/social/youtube.svg"
-import snapchatIcon from "../../assets/social/snapchat.svg"
 import linkedinIcon from "../../assets/social/linkedin.svg"
-import upworkIcon from "../../assets/social/upwork.svg"
+import instagramIcon from "../../assets/social/instagram.svg"
+import youtubeIcon from "../../assets/social/youtube.svg"
+import twitterIcon from "../../assets/social/twitter.svg"
+import pinterestIcon from "../../assets/social/pinterest.svg"
 
 export const socialIcon = {
   facebookIcon: facebookIcon,
-  instagramIcon: instagramIcon,
-  twitterIcon: twitterIcon,
-  youtubeIcon: youtubeIcon,
-  snapchatIcon: snapchatIcon,
   linkedinIcon: linkedinIcon,
-  tiktokIcon: tiktokIcon,
-  upworkIcon: upworkIcon,
+  instagramIcon: instagramIcon,
+  youtubeIcon: youtubeIcon,
+  twitterIcon: twitterIcon,
+  pinterestIcon: pinterestIcon,
 }
